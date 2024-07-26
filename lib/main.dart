6f388f0c784be2599072e6cfb1985f73affe071e
atsalrizandri <atsalrizandrii@gmail.com>
2024-07-27 01:42:08 +0700

    feat: codelab 3 - featured tourist attractions list

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:wisatabandung_dicoding/detail_screen.dart';
+import 'package:wisatabandung_dicoding/main_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Wisata Bandung',
       theme: ThemeData(),
-      home: const DetailScreen(),
+      home: const MainScreen(),
     );
   }
 }
